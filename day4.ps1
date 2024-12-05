09b47019ff559887e53d02a66c4c4bb7b31cdb46
Vortiago <atle@havso.net>
2024-12-05 01:36:14 +0100

    Improvements!

--- day4.ps1
+++ day4.ps1
@@ -21,6 +21,25 @@ else {
 
 $charMap = $inputData.Split([System.Environment]::NewLine) | ForEach-Object { , [char[]]$_}
 
+class Direction {
+    [int] $RowDirection
+    [int] $ColumnDirection
+
+    Direction($rowDirection, $columnDirection) {
+        $this.RowDirection = $rowDirection
+        $this.ColumnDirection = $columnDirection
+    }
+}
+
+$NorthDirection = [Direction]::new(-1, 0)
+$NorthEastDirection = [Direction]::new(-1, 1)
+$EastDirection = [Direction]::new(0, 1)
+$SouthEastDirection = [Direction]::new(1, 1)
+$SouthDirection = [Direction]::new(1, 0)
+$SouthWestDirection = [Direction]::new(1, -1)
+$WestDirection = [Direction]::new(0, -1)
+$NorthWestDirection = [Direction]::new(-1, -1)
+
 function Get-2DArrayElement {
     param(
         [array]$Array,
@@ -36,49 +55,28 @@ function Get-2DArrayElement {
     return $Array[$RowIndex][$ColumnIndex]
 }
 
+function CheckDirection([Direction]$direction, $row, $column) {
+    try {
+        if ((Get-2DArrayElement $charMap ($row + $direction.RowDirection) ($column + $direction.ColumnDirection)) -eq "M" -and
+            (Get-2DArrayElement $charMap ($row + ($direction.RowDirection * 2)) ($column + ($direction.ColumnDirection * 2))) -eq "A" -and 
+            (Get-2DArrayElement $charMap ($row + ($direction.RowDirection * 3)) ($column + ($direction.ColumnDirection * 3))) -eq "S") {
+            return 1
+        }
+    } catch { }
+
+    return 0
+}
+
 function ExplodeChar($row, $column) {
     $foundXMAS = 0
-
-    try {
-        if ((Get-2DArrayElement $charMap $row ($column + 1)) -eq "M" -and (Get-2DArrayElement $charMap $row ($column + 2)) -eq "A" -and (Get-2DArrayElement $charMap $row ($column + 3)) -eq "S") {
-            $foundXMAS++
-        }
-    } catch {}
-    try {
-        if ((Get-2DArrayElement $charMap $row ($column - 1)) -eq "M" -and (Get-2DArrayElement $charMap $row ($column - 2)) -eq "A" -and (Get-2DArrayElement $charMap $row ($column - 3)) -eq "S") {
-            $foundXMAS++
-        }
-    } catch {}
-    try {        
-        if ((Get-2DArrayElement $charMap ($row + 1) $column) -eq "M" -and (Get-2DArrayElement $charMap ($row + 2) $column) -eq "A" -and (Get-2DArrayElement $charMap ($row + 3) $column) -eq "S") {
-            $foundXMAS++
-        }
-    } catch {}
-    try {
-        if ((Get-2DArrayElement $charMap ($row - 1) $column) -eq "M" -and (Get-2DArrayElement $charMap ($row - 2) $column) -eq "A" -and (Get-2DArrayElement $charMap ($row - 3) $column) -eq "S") {
-            $foundXMAS++
-        }
-    } catch {}
-    try {
-        if ((Get-2DArrayElement $charMap ($row + 1) ($column + 1)) -eq "M" -and (Get-2DArrayElement $charMap ($row + 2) $($column + 2)) -eq "A" -and (Get-2DArrayElement $charMap ($row + 3) $($column + 3)) -eq "S") {
-            $foundXMAS++
-        }
-    } catch {}
-    try {
-        if ((Get-2DArrayElement $charMap ($row + 1) ($column - 1)) -eq "M" -and (Get-2DArrayElement $charMap ($row + 2) $($column - 2)) -eq "A" -and (Get-2DArrayElement $charMap ($row + 3) $($column - 3)) -eq "S") {
-            $foundXMAS++
-        }
-    } catch {}
-    try {
-        if ((Get-2DArrayElement $charMap ($row - 1) ($column + 1)) -eq "M" -and (Get-2DArrayElement $charMap ($row - 2) $($column + 2)) -eq "A" -and (Get-2DArrayElement $charMap ($row - 3) $($column + 3)) -eq "S") {
-            $foundXMAS++
-        }
-    } catch {}
-    try {
-        if ((Get-2DArrayElement $charMap ($row - 1) ($column - 1)) -eq "M" -and (Get-2DArrayElement $charMap ($row - 2) $($column - 2)) -eq "A" -and (Get-2DArrayElement $charMap ($row - 3) $($column - 3)) -eq "S") {
-            $foundXMAS++
-        }
-    } catch {}
+    $foundXMAS += (CheckDirection $NorthDirection $row $column)
+    $foundXMAS += (CheckDirection $NorthEastDirection $row $column)
+    $foundXMAS += (CheckDirection $EastDirection $row $column)
+    $foundXMAS += (CheckDirection $SouthEastDirection $row $column)
+    $foundXMAS += (CheckDirection $SouthDirection $row $column)
+    $foundXMAS += (CheckDirection $SouthWestDirection $row $column)
+    $foundXMAS += (CheckDirection $WestDirection $row $column)
+    $foundXMAS += (CheckDirection $NorthWestDirection $row $column)
 
     return $foundXMAS
 }
